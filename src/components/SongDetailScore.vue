9532429af8a4ba7569c467758ed73cdc595bcf52
Amy Y <ctrlalternativedelete@gmail.com>
2020-06-28 22:47:14 +1000

    added song display

---
+++ src/components/SongDetailScore.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="score">
+        {{scoreObj.score}}
+        {{scoreObj.difficulty}}
+        Otter.jy126
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SongList',
+  components: {
+  },
+  props: {
+    'scoreObj': Object
+  },
+  mounted() {
+    console.log(this.$props.scoreObj);
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.score {
+    margin-top: 10px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/SongDetail.vue
+++ src/views/SongDetail.vue
@@ -3,28 +3,39 @@
     Name: {{ SongObj.title }} <br>
     By: {{ SongObj.artist }} <br>
     Charter: {{ SongObj.charter }} <br>
+    <SongDetailScore v-for="score in SongScoreList" :key="score.score" :scoreObj="score"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import SSAPI from '@/modules/module.api.js'
+import SongDetailScore from '@/components/SongDetailScore.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
-
+    SongDetailScore
   },
   data: function () {
     return {
         SpinshareReference: this.$route.params.SpinshareReference,
-        SongObj: {}
+        SongObj: {},
+        SongScoreList: []
     }
   },
   mounted() {
       let ssapi = new SSAPI;
       ssapi.getSongDetail(this.$data.SpinshareReference).then(e => {
           this.$data.SongObj = e.data
+          axios.get('http://localhost:3000/getScores?search='+ this.$data.SpinshareReference)
+          .then(res => {
+            this.$data.SongScoreList = res.data
+            this.$data.SongScoreList.sort(function(a, b) {
+                return parseFloat(b.score) - parseFloat(a.score);
+            });
+          })
       });
   },
   methods: {
